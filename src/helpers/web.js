9d3cef0ef5e811493e72052ebb3fb731d910480d
prime-infinity <soe.a.prime@gmail.com>
2022-06-08 10:52:03 -0700

    third level cat management

--- src/helpers/web.js
+++ src/helpers/web.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const backendHost = "http://localhost:2000/api/";
+//const backendHost = "http://localhost:2000/api/";
+const backendHost = "https://favsadmin.herokuapp.com/api/";
 
 //login admin
 export function login(data) {
